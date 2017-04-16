170622ab9f3bac24aa0d945f4623dfeac3e20b0e
Andrew Beers <andybeers@gmail.com>
2017-04-16 16:14:16 -0700

    Added POST and GET all routes for servers, with tests.

--- front_end/server/lib/app.js
+++ front_end/server/lib/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const servers = require('./routes/servers');
+
 app.use(morgan('dev'));
+
+app.use('/api/servers', servers);
 
 //eslint-disable-next-line
 app.get('/', (req, res, next) => {
   res.send({ message: 'Hello world!'});
 });
+
 
 module.exports = app;
