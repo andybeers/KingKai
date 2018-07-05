dc19ca7754cbef9ff71b02379a5ada25fc6caff5
Andrew Beers <andybeers@gmail.com>
2018-07-05 11:54:07 -0400

    bump versions, include prettier, lotsa config

--- king_kai/app/src/components/Dashboard/index.js
+++ king_kai/app/src/components/Dashboard/index.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import { array } from 'prop-types'
-import Servers from '../Servers'
-import Snapshots from '../Snapshots'
+import React, { Component } from 'react'
+import Servers from './Servers'
+// import Snapshots from '../Snapshots'
 import AlertTest from '../AlertTest'
 import './dashboard.css'
 
-export default function Dashboard(props) {
-  return (
-    <div className='wrapper'>
-      <Servers servers={props.servers} />
-      <Snapshots snapshots={props.snapshots} hostName={props.snapshots.length ? props.snapshots[0].HOST_ID : 'Nope'} />
-      <AlertTest />
-    </div>
-  )
+class Dashboard extends Component {
+  state = {
+    servers: [],
+  }
+
+  componentDidMount() {
+    this.fetchServers()
+    // this.fetchSnapshots()
+    // this._timerId = setInterval(() => {
+    //   this.fetchServers()
+    // }, 6000)
+  }
+
+  fetchServers() {
+    console.log('Fetching data...')
+    this.setState({ loading: true })
+    fetch('http://localhost:9000/api/servers')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          servers: res,
+          serverError: '',
+          loading: false,
+        })
+      })
+      .catch(err => {
+        this.setState({
+          serverError: 'Servers done borked',
+          loading: false,
+        })
+      })
+  }
+
+  render() {
+    const { servers } = this.state
+    return (
+      <div className="wrapper">
+        <Servers servers={servers} />
+        {/* <Snapshots snapshots={props.snapshots} hostName={props.snapshots.length ? props.snapshots[0].HOST_ID : 'Nope'} /> */}
+        <AlertTest />
+      </div>
+    )
+  }
 }
 
-Dashboard.propTypes = {
-  servers: array,
-  snapshots: array
-}
+export default Dashboard
